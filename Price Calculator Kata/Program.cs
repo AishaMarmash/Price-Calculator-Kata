797ae1eafa152aea8fe3b740710b37b759c198fe
Aisha Marmash <aishamarmash@gmail.com>
2022-03-05 22:53:52 +0200

    basic structure for Product class and tax calculation

---
+++ Price Calculator Kata/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_Calculator_Kata
+{
+    internal class Program
+    {
+       public static void Main()
+       {
+            Product.Tax = 21;
+            Product product = new Product("The Little Prince",12345,20.25);
+            Console.WriteLine(product.ProductPriceInformation());
+       }
+
+    }
+}
