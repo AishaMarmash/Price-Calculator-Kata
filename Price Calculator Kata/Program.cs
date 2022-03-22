5343f68eff9ace95e55e7057103e65a6ceddb10c
AishaMarmash <54311621+AishaMarmash@users.noreply.github.com>
2022-03-22 16:48:07 +0300

    Update for main method

--- Price Calculator Kata/Program.cs
+++ Price Calculator Kata/Program.cs
@@ -13,20 +13,17 @@ namespace Price_Calculator_Kata
         {
             var productName = "The Little Prince";
             var productUpc = 12345;
-            string cost = "20.25 USD";
-            var items = cost.Split(" ");
-            Currancy.Type = items[1];
-            double productPrice = double.Parse(items[0]);
-            Product product = new Product(productName, productUpc, productPrice);
+            double cost = 20.25;
+            Product product = new Product(productName, productUpc, cost);
             _products.Add(product);
             Tax.TaxPercentage = 21;
             UniversalDiscount.DiscountPercentage = 15;
-            UniversalDiscount.When = "after";
+            UniversalDiscount.When = "before";
             UPCDiscount.setDiscountPercentage(_products, 12345, 7);
             UPCDiscount.When = "after";
             Expenses expences = new Expenses(product);
-            //Expenses.Add("Transport","3%");
-           // Expenses.Add("Packagig","1%");
+            Expenses.Add("Transport","3%");
+            Expenses.Add("Packagig","1%");
             Price.DiscountWay = "additave";
             Cap.CapValue = "30%";
             product.CurrentPrice = Price.CalculatePrice(product);
